0bb17dc123c841ad0daa54b5b2d9723541b80687
David Ruiz <david.ruiz@internourbano.com.ec>
2023-10-10 12:29:02 -0500

    Login and register design

--- Frontend/src/app/auth/login/login.component.ts
+++ Frontend/src/app/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, Renderer2 } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,14 +7,26 @@ import { Component, Renderer2 } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
-  constructor(private renderer: Renderer2) {}
+  constructor(
+    private renderer: Renderer2,
+    private router: Router,
+    private route: ActivatedRoute
+    ) {}
 
   togglePasswordVisibility() {
     const passwordInput = this.renderer.selectRootElement('#password-input');
-    if (passwordInput.getAttribute('type') === 'password') {
-      this.renderer.setAttribute(passwordInput, 'type', 'text');
-    } else {
-      this.renderer.setAttribute(passwordInput, 'type', 'password');
-    }
+    const passwordIcon = this.renderer.selectRootElement('.icon-password');
+    const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password'
+    const remove = passwordInput.getAttribute('type') === 'password' ? 'fa-eye' : 'fa-eye-slash'
+    const add = passwordInput.getAttribute('type') === 'password' ? 'fa-eye-slash' : 'fa-eye'
+    this.renderer.setAttribute(passwordInput, 'type', type);
+    this.renderer.removeClass(passwordIcon, remove);
+    this.renderer.addClass(passwordIcon, add);
+  }
+
+  signup(){
+    // Animaciones
+    // Redireccion
+    this.router.navigate(['../register'], { relativeTo: this.route })
   }
 }
--- Frontend/src/app/auth/register/register.component.ts
+++ Frontend/src/app/auth/register/register.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
+  constructor(
+    private renderer: Renderer2,
+    private router: Router,
+    private route: ActivatedRoute
+    ) {}
 
+  togglePasswordVisibility(prefix = '') {
+    const passId = '#' + prefix + 'password-input'
+    const iconId = '.' + prefix + 'icon-password'
+    const passwordInput = this.renderer.selectRootElement(passId);
+    const passwordIcon = this.renderer.selectRootElement(iconId);
+    const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password'
+    const remove = passwordInput.getAttribute('type') === 'password' ? 'fa-eye' : 'fa-eye-slash'
+    const add = passwordInput.getAttribute('type') === 'password' ? 'fa-eye-slash' : 'fa-eye'
+    this.renderer.setAttribute(passwordInput, 'type', type);
+    this.renderer.removeClass(passwordIcon, remove);
+    this.renderer.addClass(passwordIcon, add);
+  }
+
+  signin(){
+    // Animaciones
+    // Redireccion
+    this.router.navigate(['../login'], { relativeTo: this.route })
+  }
 }
