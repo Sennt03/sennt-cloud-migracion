97cbccf1e5e8dbebae102d1712425e62532a1d99
David Ruiz <david.ruiz@internourbano.com.ec>
2023-12-21 12:47:53 -0500

    Dashboard template init

--- Frontend/src/app/main/dashboard/dashboard.component.ts
+++ Frontend/src/app/main/dashboard/dashboard.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit{
 
+  paths: {
+    path: string,
+    link: string
+  }[] = []
+  currentUrl = ''
+
+  constructor(
+    private router: Router
+  ){
+  }
+
+  ngOnInit(): void {
+    this.processURL()
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.processURL();
+      }
+    });
+  }
+
+  processURL() {
+    const data = []
+    const details = this.router.url.split('/').slice(2)
+    details.forEach((path, i) => {
+      const ruta = details.slice(0, i + 1).join('/')
+      const detail = {
+        path,
+        link: `/r/${ruta}`
+      }
+      
+      data.push(detail)
+    });
+    this.paths = this.router.url.split('/').length > 2 ? data : [];
+  }
+  
 }
