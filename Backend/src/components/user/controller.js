1f2da55e554d669a8212a894000a1863d506c6fa
David Ruiz <david.ruiz@internourbano.com.ec>
2023-12-21 09:32:38 -0500

    Add analytics profile backend

--- Backend/src/components/user/controller.js
+++ Backend/src/components/user/controller.js
@@ -2,9 +2,14 @@
 const Model = require('./model')
 const Store = require('../../db/store')
 const store = new Store(Model)
+const cloudController = require('../cloud/controller')
 
-function getUser(id){
-    return store.findOneById(id, {password: 0})
+async function getUser(id){
+    const analytics = await cloudController.analitycsData(id)
+    const user = await store.findOneById(id, {password: 0})
+    user._doc.analytics = analytics
+
+    return user
 }
 
 module.exports = {
--- Frontend/src/app/core/models/user.models.ts
+++ Frontend/src/app/core/models/user.models.ts
@@ -1,7 +1,12 @@
 export interface LsUser{
     _id: string,
     username: string,
-    email: string
+    email: string,
+    analytics: {
+        files: number,
+        folders: number,
+        totalSize: string
+    }
 }
 
 export type LsField = 'name' | 'username' | 'email'
