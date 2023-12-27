c1744bb4a25b510bb022460abfe211fcee5fa611
David Ruiz <david.ruiz@internourbano.com.ec>
2023-12-27 11:20:01 -0500

    add my error

--- Backend/src/components/user/routes.js
+++ Backend/src/components/user/routes.js
@@ -17,7 +17,7 @@ router.get('/', verifyToken, async (req, res, next) => {
 router.post('/changePassword', verifyToken, changePasswordValidator, async (req, res, next) => {
     try{
         await controller.changePassword(req.user._id, req.body)
-        response(req, res, {message: 'Password updated successfully'})
+        response.success(req, res, {message: 'Password updated successfully'})
     }catch(err){
         next(err)
     }
