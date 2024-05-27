a5b93143f257f6b854884937899fa335b112888e
David <davidrsmk144@gmail.com>
2024-05-27 00:50:46 -0500

    add build prod

--- Frontend/src/enviroment/enviroment.ts
+++ Frontend/src/enviroment/enviroment.ts
@@ -1,7 +1,9 @@
 export const environment = {
     production: false,
-    url_base: 'http://localhost:3000',
-    url_api: 'http://localhost:3000/api'
+    url_base: '/',
+    url_api: '/api',
+    // url_base: 'http://localhost:3000',
+    // url_api: 'http://localhost:3000/api'
     // url_base: 'http://192.168.100.75:3000',
     // url_api: 'http://192.168.100.75:3000/api'
     // url_base: 'https://social.davidruiz.online',
