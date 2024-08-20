2c7be9db395e814c6f2125bc3c2abd5e1d1cc36d
David Ruiz <david.ruiz@internourbano.com.ec>
2024-08-20 13:51:12 -0500

    build prod

--- Frontend/src/enviroment/enviroment.ts
+++ Frontend/src/enviroment/enviroment.ts
@@ -1,15 +1,9 @@
 export const environment = {
     production: false,
-    // url_base: '/',
-    // url_api: '/api',
+    url_base: '/',
+    url_api: '/api',
     // url_base: 'http://localhost:3000',
     // url_api: 'http://localhost:3000/api'
-    // url_base: 'http://192.168.100.75:3000',
-    // url_api: 'http://192.168.100.75:3000/api'
-    // url_base: 'https://social.davidruiz.online',
+    // url_base: 'https://social.davidruiz.online/',
     // url_api: 'https://social.davidruiz.online/api'
-    // url_base: 'http://ec2-18-189-1-134.us-east-2.compute.amazonaws.com:3005',
-    // url_api: 'http://ec2-18-189-1-134.us-east-2.compute.amazonaws.com:3005/api'
-    url_base: 'https://social.davidruiz.online/',
-    url_api: 'https://social.davidruiz.online/api'
 };
